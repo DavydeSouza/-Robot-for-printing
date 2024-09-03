6d68080d09ac9cecd5cce570799b49f9e6e80351
Davy de Souza <davydesouza02@gmail.com>
2024-09-03 09:07:48 -0300

    Puxando IPS do Bancos de Dados

--- project/config.py
+++ project/config.py
@@ -1,27 +1,25 @@
 import pyodbc
 
-# Passo 1: Estabeleça a conexão com o banco de dados
-conn_str = (
-    'DRIVER={SQL Server};'
-    'SERVER=10.10.13.250;'  # Remova as barras e deixe apenas o endereço IP
-    'DATABASE=Impressora;'
-    'UID=netazzurra;'
-    'PWD=Azzurra@@2023'
-)
+def obter_ips_do_banco():
+    conn_str = (
+        'DRIVER={SQL Server};'
+        'SERVER=10.10.13.250;'
+        'DATABASE=Impressora;'
+        'UID=netazzurra;'
+        'PWD=Azzurra@@2023'
+    )
+    ips = []
+    
+    try:
+        # Conectando ao banco de dados
+        with pyodbc.connect(conn_str) as conn:
+            with conn.cursor() as cursor:
+                # Executando a consulta para pegar os IPs
+                cursor.execute('SELECT ipImp FROM impressora')  # Ajuste a consulta conforme necessário
+                rows = cursor.fetchall()
 
-try:
-    # Conectando ao banco de dados
-    with pyodbc.connect(conn_str) as conn:
-        with conn.cursor() as cursor:
-            # Executando a consulta
-            cursor.execute('SELECT * FROM impressora')  # Substitua 'nome_tabela' pela sua tabela
-
-            # Puxando os dados
-            rows = cursor.fetchall()
-
-            # Processando os dados
-            for row in rows:
-                print(row)
-
-except pyodbc.Error as e:
-    print(f"Erro na conexão ou na execução da consulta: {e}")
+                # Armazenando os IPs na lista
+                ips = [row[0] for row in rows]
+    except pyodbc.Error as e:
+        print(f"Erro ao consultar o banco de dados: {e}")
+    return ips
